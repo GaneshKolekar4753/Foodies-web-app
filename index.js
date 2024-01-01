5ad06037ba72add847f9d95e5cf797a0ca684772
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2024-01-02 01:15:47 +0530

    push build

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname,'./client/build')));
 
 app.get("*",function(req,res){
     res.sendFile(path.join(__dirname,"./client/build/index.html"));
-})
+});
 
 app.listen(port,function(err){
     if(err){
